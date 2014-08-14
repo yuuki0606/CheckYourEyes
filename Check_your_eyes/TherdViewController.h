3e41056a0a1b9947ab3bec1feb03c2e363625d4b
yuuki <yuukimatsuguchi@sumie-no-MacBook-Pro.local>
2014-08-14 13:05:47 +0900

    dgrthtrddh

---
+++ Check_your_eyes/TherdViewController.h
@@ -0,0 +1,13 @@
+//
+//  TherdViewController.h
+//  Check_your_eyes
+//
+//  Created by yuuki on 2014/08/14.
+//  Copyright (c) 2014年 yuuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TherdViewController : UIViewController
+
+@end
---
+++ Check_your_eyes/TherdViewController.m
@@ -0,0 +1,49 @@
+//
+//  TherdViewController.m
+//  Check_your_eyes
+//
+//  Created by yuuki on 2014/08/14.
+//  Copyright (c) 2014年 yuuki. All rights reserved.
+//
+
+#import "TherdViewController.h"
+
+@interface TherdViewController ()
+
+@end
+
+@implementation TherdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
